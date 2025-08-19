9280dc5d705bfddb7475c2dd35031f414119afb1
satyco2111-beep <satyco2111@gmail.com>
2025-08-19 16:47:17 +0530

    correct code

--- app/routes/api.app.create-order.js
+++ app/routes/api.app.create-order.js
@@ -11,7 +11,7 @@ export const loader = async ({ request }) => {
   try {
     const sessions = await db.session.findMany(); // Prisma syntax to get all sessions
     return {
-      message: "Congrats on your SKU app deployed! raajveer",
+      message: "Congrats on your SKU app deployed!",
       name: "Inventory App",
       sessions,
     };
