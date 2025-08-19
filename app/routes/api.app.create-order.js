9d97fdffa7fce0706b0b8e0422e61382179418cd
satyco2111-beep <satyco2111@gmail.com>
2025-08-19 15:41:11 +0530

    sesion vairable added

--- app/routes/api.app.create-order.js
+++ app/routes/api.app.create-order.js
@@ -1,5 +1,5 @@
-// import db from "../db.server";
+import db from "../db.server";
 export const loader = async ({ request }) => {
-//   const session = await db.session.findFirst();
-  return {"massage":" Congrats on  your sku app deployed !","name":"inventory app"};
+  const session = await db.session.findFirst();
+  return {"massage":" Congrats on  your sku app deployed !","name":"inventory app","session":session};
 };
